31bd405bd588ded278752f81cd441ce8afd04981
anara01 <anaraerkebaeva2@gmail.com>
2022-12-09 19:47:23 +0300

    page user

---
+++ lib/screens/user.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:fruits_app/widgets/text_widget.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/dark_theme_provider.dart';
+
+class UserScreen extends StatefulWidget {
+  const UserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RichText(
+                  text: TextSpan(
+                    text: 'Hi,   ',
+                    style: const TextStyle(
+                      color: Colors.cyan,
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'MyName',
+                        style: TextStyle(
+                          color: color,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            print('My name is pressed');
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+                TextWidget(
+                  text: 'Email@email.com',
+                  color: color,
+                  textSize: 18,
+                  //isTitle: true,
+                ),
+                const SizedBox(height: 20),
+                const Divider(
+                  thickness: 2,
+                ),
+                const SizedBox(height: 20),
+                _listTitles(
+                  title: 'Address',
+                  subtitle: 'Subtitle here',
+                  icon: IconlyLight.profile,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTitles(
+                  title: 'Orders',
+                  icon: IconlyLight.bag,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTitles(
+                  title: 'Wishlist',
+                  icon: IconlyLight.heart,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTitles(
+                  title: 'Viewed',
+                  icon: IconlyLight.show,
+                  onPressed: () {},
+                  color: color,
+                ),
+                _listTitles(
+                  title: 'Forget password',
+                  icon: IconlyLight.unlock,
+                  onPressed: () {},
+                  color: color,
+                ),
+                SwitchListTile(
+                  title: TextWidget(
+                    text: themeState.getDarkTheme ? 'Dark mode' : 'Light mode',
+                    color: color,
+                    textSize: 22,
+                    //isTitle: true,
+                  ),
+                  secondary: Icon(themeState.getDarkTheme
+                      ? Icons.dark_mode_outlined
+                      : Icons.light_mode_outlined),
+                  onChanged: (bool value) {
+                    setState(() {
+                      themeState.setDarkTheme = value;
+                    });
+                  },
+                  value: themeState.getDarkTheme,
+                ),
+                _listTitles(
+                  title: 'Logout',
+                  icon: IconlyLight.logout,
+                  onPressed: () {},
+                  color: color,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _listTitles({
+    required String title,
+    String? subtitle,
+    required IconData icon,
+    required Function onPressed,
+    required Color color,
+  }) {
+    return ListTile(
+      title: TextWidget(
+        text: title,
+        color: color,
+        textSize: 22,
+        //isTitle: true,
+      ),
+      subtitle: TextWidget(
+        text: subtitle == null ? "" : subtitle,
+        color: color,
+        textSize: 18,
+      ),
+      leading: Icon(icon),
+      trailing: const Icon(IconlyLight.arrowRight2),
+      onTap: () {
+        onPressed();
+      },
+    );
+  }
+}
