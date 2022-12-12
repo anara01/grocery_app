dd77f9caf774e0cb358009a86c09e2d8b916abbd
anara01 <anaraerkebaeva2@gmail.com>
2022-12-12 14:16:35 +0300

    cart page

--- lib/screens/btm_bar.dart
+++ lib/screens/btm_bar.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
-import 'package:fruits_app/screens/card.dart';
 import 'package:fruits_app/screens/categories.dart';
 import 'package:fruits_app/screens/home_screen.dart';
 import 'package:fruits_app/screens/user.dart';
 import 'package:provider/provider.dart';
 
 import '../provider/dark_theme_provider.dart';
+import 'cart/cart_screen.dart';
 
 class BottomBarScreen extends StatefulWidget {
   const BottomBarScreen({Key? key}) : super(key: key);
